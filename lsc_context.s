77fedfa1d5df49fd61f6a8221d35420f6c5980a7
Xianguang-Zhou <xianguang.zhou@outlook.com>
2018-01-11 22:20:01 +0800

    add license header

--- lsc_context.s
+++ lsc_context.s
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018, Xianguang Zhou <xianguang.zhou@outlook.com>. All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 	.globl	lsc_start_context
 	.type	lsc_start_context, @function
 lsc_start_context:
